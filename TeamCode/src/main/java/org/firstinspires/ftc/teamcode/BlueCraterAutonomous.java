fe56b71b884c6ffea591bb0fb367fbe32001f5c0
Yimo Tank <yimo.liu@mail.com>
2018-11-16 17:58:04 -0500

    Finished up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueCraterAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueCraterAutonomous.java
@@ -69,9 +69,10 @@ public class BlueCraterAutonomous extends LinearOpMode {
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
+        robot.lowerLift();
         robot.moveInches(16.5, 0.65, 15);
 
-        /*while (robot.sensorDistance.getDistance(DistanceUnit.INCH) > 5) {
+
             //strafetime
             //if: sensorDistance is <5
             //then test for yellow
@@ -81,18 +82,19 @@ public class BlueCraterAutonomous extends LinearOpMode {
             //else strafe while Sensordistance <5
 
 
-            robot.rotate(-84, 0.25);
+        robot.rotate(-84, 0.25);
 
-            robot.moveInches(50, 0.65, 15);
-            robot.rotate(-30, 0.25);
+        robot.moveInches(50, 0.65, 15);
+        robot.rotate(-30, 0.25);
 
-            robot.moveInches(-35, 0.65, 15);
+        robot.moveInches(-35, 0.65, 15);
+        robot.dropMarker();
             //robot.rotate(-3,0.25);
 
-            robot.moveInches(-80, 0.85, 15);
+        robot.moveInches(-80, 0.85, 15);
 
 
-        }*/
+
     }
 
 }
